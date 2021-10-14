0161cca8e27d59ca677f882bbde572da7255ebda
Moulee <mouleeswaran@agentz.ai>
2021-10-15 00:18:20 +0530

    Logout Added

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,21 +8,38 @@ class Main extends React.Component {
     this.state = {};
   }
 
-  componentWillMount(){
-    if(localStorage.getItem("username")===null){
+  componentWillMount() {
+    if (localStorage.getItem("username") === null) {
       const {
         history: { push },
       } = this.props;
-      push('/');
+      push("/");
     }
   }
+
+  handleLogout = () => {
+    localStorage.removeItem("username");
+    const {
+      history: { push },
+    } = this.props;
+    push("/");
+  };
+
   render() {
     // const { data } = this.props.location;
     return (
       <div className="space-y-5 h-screen flex flex-col">
-        <h2 className="text-4xl font-semibold text-indigo-900">
-          Sports Management
-        </h2>
+        <div className="flex justify-between items-center">
+          <h2 className="text-4xl font-semibold text-blue-800">
+            Sports Management
+          </h2>
+          <button
+            class="bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2 mt-2"
+            onClick={this.handleLogout}
+          >
+            Logout
+          </button>
+        </div>
         <Tab />
       </div>
     );
