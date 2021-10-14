d29d4212bc97eb33f7dfab9bc9dfc5d79127bcd2
Moulee <mouleeswaran@agentz.ai>
2021-10-14 12:36:24 +0530

    Initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+        <h2 className="text-4xl font-semibold text-indigo-900">
+          Sports Management
+        </h2>
+    );
+  }
+}
+
+export default Main;
